f6a59694df8970f440f2dcd728e9c3602891b7fe
MamtazDev <akter.mstmamtaz@gmail.com>
2024-03-09 19:17:11 +0600

    adf

--- src/app/dashboard/notification/page.tsx
+++ src/app/dashboard/notification/page.tsx
@@ -1,10 +1,57 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import instance from '@/hooks/instance'
 
 const notification = () => {
+
+
+  
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [userDatas, setUserDatas] = useState<any>({})
+
+
+
+  useEffect(() => {
+
+    let userDataString;
+    if (typeof window !== undefined) {
+        userDataString = localStorage.getItem('user');
+    }
+    if (userDataString) {
+        const userData = JSON.parse(userDataString);
+        setUserDatas(userData?.user);
+    }
+}, []);
+
+
+  async function fetchData() {
+  
+
+    setIsLoading(true)
+    try {
+        if (userDatas._id) {
+            const managerIds = userDatas._id;
+            const response = await instance.get(`/api/authorization/checkAuth/65ec516dadf21eea2e8cf2de}`);
+            setData(response.data.data);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    } finally {
+        setIsLoading(false)
+    }
+}
+
+useEffect(() => {
+    fetchData();
+}, [userDatas]);
+
+
   return (
     <div>
 
-      
+      <p>Your approval related Information</p>
     </div>
   )
 }
