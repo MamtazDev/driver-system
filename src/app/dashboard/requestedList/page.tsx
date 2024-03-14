58bdf378d654d527b28555bafcf3fc59e68aeb45
tonni-akter-dev <tanniakhter123@gmail.com>
2024-03-14 14:51:34 +0600

    DFDSF

--- src/app/dashboard/requestedList/page.tsx
+++ src/app/dashboard/requestedList/page.tsx
@@ -285,7 +285,7 @@ const RequestedList = () => {
                                       {requests?.user?.phoneNumber}
                                     </td>
 
-                                    <td className="w-[130px]">
+                                    <td className="w-fit">
                                       <select
                                         className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                                         onChange={(e) =>
